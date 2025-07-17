194f93f79dfaa5c3f0b5f70b16276a0c9ce1fbee
ankitabhagade21 <ankitabhagde2173@gmail.com>
2025-07-17 19:58:34 +0530

    Add files via upload

---
+++ lab1/assignment1.js
@@ -0,0 +1,5 @@
+var a ="Hello, this is my first Node.js program!";
+var b="I am excited to learn more about Node.js!";
+console.log(a);
+console.log(b);
+console.log(a+b)
---
+++ lab1/assignment2.js
@@ -0,0 +1,7 @@
+
+var a="Ankita";
+var b="Bhagade";
+var c="MCA-I";
+console.log("Length of first string = " + a.length);
+console.log("Length of second string = " + b.length);
+console.log("Length of third string = " + c.length)
